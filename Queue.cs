a18708ade631d4f4a1951615c2d3ad495cca363f
Snarie <144214499+Snarie@users.noreply.github.com>
2024-06-12 01:54:21 +0200

    Added comments + code to Queue class

--- Queue.cs
+++ Queue.cs
@@ -3,12 +3,14 @@ using Pentotris.Shapes;
 
 namespace Pentotris
 {
+    /// <summary>
+    /// Manages the queue of upcoming blocks in the game.
+    /// </summary>
     internal class BlockQueue
     {
-        internal BlockQueue()
-        {
-            NextBlock = RandomBlock();
-        }
+        /// <summary>
+        /// The array of possible tetromino blocks.
+        /// </summary>
         private readonly Block[] tetrominos = new Block[]
         {
             new ITetromino(),
@@ -20,23 +22,46 @@ namespace Pentotris
             new ZTetromino()
         };
 
+        /// <summary>
+        /// Random number generator for selecting the next block.
+        /// </summary>
         private readonly Random random = new();
 
+        /// <summary>
+        /// Gets the next block to be used in the game.
+        /// </summary>
         internal Block NextBlock { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlockQueue"/> class.
+        /// </summary>
+        internal BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        /// <summary>
+        /// Returns a random block from the tetrominos array.
+        /// </summary>
+        /// <returns>A random <see cref="Block"/>.</returns>
         private Block RandomBlock()
         {
             return tetrominos[random.Next(tetrominos.Length)];
         }
 
+        /// <summary>
+        /// Gets the current block and updates the queue with a new block.
+        /// Ensures the same block is not returned consecutively.
+        /// </summary>
+        /// <returns>The current <see cref="Block"/>.</returns>
         public Block GetAndUpdate()
         {
             Block block = NextBlock;
-            // Make sure we never get the same block 2 tiems in a row
             do
             {
                 NextBlock = RandomBlock();
             }
+            // Ensure we don't get the same block consecutively
             while (block.Id == NextBlock.Id);
 
             return block;
